eadf1a90db506b48e3fc3ba4b7c56ebda139c979
fahiid33 <trey34809@gmail.com>
2023-11-12 22:06:04 +0100

    chat init

---
+++ back-end/src/Chat/messages.gateway.ts
@@ -0,0 +1,36 @@
+import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer } from '@nestjs/websockets';
+import { MessagesService } from './messages.service';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+import { Server , Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors : {
+    origin : '*' ,// Allow all origins
+  },
+})
+export class MessagesGateway {
+
+  @WebSocketServer() // to be able to send messages to all the clients
+  server : Server; // reference to the socket.io server under the hood
+
+  constructor(private readonly messagesService: MessagesService) {}
+
+  @SubscribeMessage('createMessage') // to be able to send new messages
+  async create(@MessageBody() createMessageDto: CreateMessageDto) {
+    const message = await this.messagesService.create(createMessageDto);
+    
+    this.server.emit('message' , message); // emit events to all connected clients
+
+    return message;
+  }
+
+  @SubscribeMessage('findAllMessages') // to be able to see the old messages 
+  findAll() {
+    return this.messagesService.findAll();
+  }
+  @SubscribeMessage('join')
+  joinRoom(){
+    // TODO: join the room
+  }
+}
---
+++ back-end/src/Chat/messages.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { CreateMessageDto } from './dto/create-message.dto';
+import { UpdateMessageDto } from './dto/update-message.dto';
+import { Message } from './entities/message.entity';
+
+@Injectable()
+export class MessagesService {
+
+  messages : Message[] = [ {name : 'Fahid' , text : 'Wach Wach!'} ];
+  create(createMessageDto: CreateMessageDto) {
+    const message = {...createMessageDto}
+    this.messages.push(message); // TODO : improve this method  
+    return message;
+  }
+
+  findAll() {
+    return this.messages; // TODO : add a query to select all from the messages table
+  }
+}
